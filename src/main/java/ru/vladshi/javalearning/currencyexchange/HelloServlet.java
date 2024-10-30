294d27f5c4354d2ae8dd75c728bca211fc9d385f
VladShi <g3rc@yandex.ru>
2024-10-30 12:23:45 +0300

    Add info to README.MD and postman collections for
    API testing. And add to HelloServlet(html index page) a list of all
    endpoints.

--- src/main/java/ru/vladshi/javalearning/currencyexchange/HelloServlet.java
+++ src/main/java/ru/vladshi/javalearning/currencyexchange/HelloServlet.java
@@ -15,10 +15,36 @@ public class HelloServlet extends HttpServlet {
         response.setContentType("text/html");
 
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<a href='/currencies'> /currencies </a>");
-        out.println("<br>");
-        out.println("<a href='/exchangeRates'> /exchangeRates </a>");
-        out.println("</body></html>");
+        out.println("""
+        <html><body>
+            <h2>Привет Пейдж</h2>
+            <h3>Старницы проетка №3 "Меняй деньга"</h3>
+            <a href='https://zhukovsd.github.io/java-backend-learning-course/projects/currency-exchange/'>
+            Ссыль на ТЗ. </a>
+            Благодарности Сергею Жукову.
+            <h4>Это api сервис, все ответы на запросе приходят в виде Json, так что тут не предусмотрен frontend <br>
+            и всё будет страшно некрасиво. Этой страницы тут вообще не должно быть и сюда не надо ходить. <br>
+            Оставлено для тех кому лень скачивать коллекции запросов для Postman, но хочется что-то потыкать.</h4>
+            <p>Получение списка всех валют: <br><a href='currencies'>/currencies </a></p>
+            <p>Получение конкретной валюты: <br><a href='currency/RUB'>/currency/RUB</a></p>
+            <p>Получение списка всех обменных курсов: <br><a href='exchangeRates'>/exchangeRates</a></p>
+            <p>Получение конкретного обменного курса: <br><a href='exchangeRate/USDRUB'>/exchangeRate/USDRUB</a></p>
+            <p>Расчёт перевода определённого количества средств из одной валюты в другую: <br>
+            <a href='exchange?from=USD&to=RUB&amount=10'>/exchange?from=USD&to=RUB&amount=10</a></p>
+            <br> Для отправки остальных методов используйте POSTMAN или что-то подобное.
+            <p>POST <b>/currencies</b> : <br>
+            Добавление новой валюты в базу. Данные передаются в теле запроса в виде полей формы
+            (x-www-form-urlencoded).
+            <br>Поля формы - <br><b>name</b><br><b>code</b><br><b>sign</b></p>
+            <p>POST <b>/exchangeRates</b> : <br>
+            Добавление нового обменного курса в базу. Данные передаются в теле запроса в виде полей формы
+            (x-www-form-urlencoded).
+            <br>Поля формы - <br><b>baseCurrencyCode</b><br><b>targetCurrencyCode</b><br><b>rate</b></p>
+            <p>PATCH <b>/exchangeRate/USDRUB</b> :<br>
+            Обновление существующего в базе обменного курса. В теле запроса в виде полей формы
+            (x-www-form-urlencoded).
+            <br>Единственное поле формы - <br><b>rate</b></p>
+        </body></html>
+        """);
     }
 }
